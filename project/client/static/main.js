406431b136a8befbce98155537dc755cf105e70a
HonzsSedlon <hanisek.sedlon@gmail.com>
2017-12-12 18:30:27 +0100

    Forgot to add base javascript

--- project/client/static/main.js
+++ project/client/static/main.js
@@ -1,5 +1,12 @@
 // custom javascript
 
-$( document ).ready(function() {
-  console.log('Sanity Check!');
-});
+$(document).ready( () => {
+
+    alertify.set('notifier', 'position', 'top-right')
+
+    let socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+
+    socket.on('client notification', (msg) => {
+        alertify.notify(msg.data, msg.result != undefined ? msg.result : "success")
+    });
+})
